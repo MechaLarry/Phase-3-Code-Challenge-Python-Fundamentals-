1d5e52b2106e999622d3c96ce5e8cdda9b53343b
MechaLarry <mecha.larry@gmail.com>
2024-09-02 01:04:38 +0300

    add the codes to run the functions

--- appfunc.py
+++ appfunc.py
@@ -1,12 +1,14 @@
 # function to add numbers
 def add_numbers(num1, num2):
     return num1 + num2
+# run
 print(add_numbers(3, 5)) #output should be 8
 
 
 # function to check if a number is even
 def is_even(num):
     return num % 2 == 0
+# run
 print(is_even(4))  # Output: True
 print(is_even(7))  # Output: False
 
@@ -14,6 +16,7 @@ print(is_even(7))  # Output: False
 # function to reverse a string
 def reverse_string(text):
     return text[::-1]
+# run
 print(reverse_string("hello"))
 
 # function to count vowels
@@ -24,6 +27,7 @@ def count_vowels(text):
         if char in vowels:
             count += 1
     return count
+# run
 print(count_vowels("Larry Mecha"))
 
 # function to calculate fuctoral
@@ -34,6 +38,7 @@ def calculate_factorial(n):
     for i in range(1, n + 1):
         factorial *= i
     return factorial
+# run
 print(calculate_factorial(9))
 
 # function to apply decorator
@@ -49,12 +54,14 @@ def sample_function():
 
 def apply_decorator(func):
     return decorator_func(func)
+# run
 sample_function()
 
 # function to sort a list of tuples by age in ascending order
 def sort_by_age(list_of_tuples):
     return sorted(list_of_tuples, key=lambda x: x[1])
 people = [("Stella", 30), ("Snappy", 25), ("Osir", 24)]
+# run
 print(sort_by_age(people))
 
 # function to take 2 dictionaries/objects as input and merges them and common keys to be summed up
@@ -68,6 +75,7 @@ def merge_dicts(dict1, dict2):
     return merged_dict
 dict1 = {"a": 1, "b": 2}
 dict2 = {"b": 3, "c": 4}
+# run 
 print(merge_dicts(dict1, dict2)) 
 
 # function that prints out a car's information
@@ -81,7 +89,7 @@ class Car:
         print(f"Car Make: {self.make}")
         print(f"Car Model: {self.model}")
         print(f"Car Year: {self.year}")
-        
+
         # run the block
 my_car = Car("Rolls Royce", "Cullinan", 2025)
 my_car.display_info()
